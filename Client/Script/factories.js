b1e87e9a878c323bd3674d953bddef6bda9b4807
Matt Randell <versioncontrol@hexaglow.com>
2015-07-27 19:22:19 +0100

    The earth is pretty

--- Client/Script/factories.js
+++ Client/Script/factories.js
@@ -2,7 +2,7 @@
 
 const THREE = require("three");
 
-function skyboxFactory(base, extension) {
+function skyboxFactory(base, extension, size) {
     let images = [
         base + "posx" + extension, base + "negx" + extension,
         base + "posy" + extension, base + "negy" + extension,
@@ -11,14 +11,31 @@ function skyboxFactory(base, extension) {
     for (let i = 0; i < images.length; i++) {
         images[i] = new THREE.MeshBasicMaterial({
             map: THREE.ImageUtils.loadTexture(images[i]),
-            side: THREE.DoubleSide
+            side: THREE.DoubleSide,
+            depthWrite: false
         });
     }
 
     let material = new THREE.MeshFaceMaterial(images);
-    return new THREE.Mesh(new THREE.BoxGeometry(500, 500, 500), material);
+    let scene = new THREE.Scene();
+    scene.skybox = new THREE.Mesh(new THREE.BoxGeometry(size, size, size), material);
+    scene.add(scene.skybox);
+    return scene;
+}
+
+function earthFactory(radius) {
+    return new THREE.Mesh(
+        new THREE.SphereGeometry(radius, 64, 64),
+        new THREE.MeshPhongMaterial({
+            map: THREE.ImageUtils.loadTexture("/earth/diffuse.jpg"),
+            normalMap: THREE.ImageUtils.loadTexture("/earth/norm.jpg"),
+            specularMap: THREE.ImageUtils.loadTexture("/earth/spec.jpg"),
+            specular: new THREE.Color("grey")
+        })
+    );
 }
 
 module.exports = {
-    skyboxFactory
+    skyboxFactory,
+    earthFactory
 };
--- Client/Script/vismod.js
+++ Client/Script/vismod.js
@@ -6,28 +6,29 @@ require("./FlyControls");
 
 let app = window.app = {};
 app.scene = new THREE.Scene();
+app.background = factories.skyboxFactory("/stars_", ".png", 1000);
 app.renderer = new THREE.WebGLRenderer({
-    canvas: document.getElementById("target")
+    canvas: document.getElementById("target"),
+    logarithmicDepthBuffer: true
 });
-app.camera = new THREE.PerspectiveCamera(50, 1, 0.1, 1000);
+app.camera = new THREE.PerspectiveCamera(50, 1, 7.5, 10000000);
+app.camera.position.set(0, 0, 8500000);
 app.scene.add(app.camera);
 
 (() => {
-    let skybox = factories.skyboxFactory("/stars_", ".png");
-    app.scene.add(skybox);
-
-    skybox.update = function () {
-        this.position.set(0, 0, 0);
-
-        app.camera.localToWorld(this.position);
-    };
-
     let controls = app.controls = new THREE.FlyControls( app.camera );
-	controls.movementSpeed = 1000;
+	controls.movementSpeed = 100000;
 	controls.domElement = app.renderer.domElement;
 	controls.rollSpeed = Math.PI / 24;
 	controls.autoForward = false;
 	controls.dragToLook = false;
+
+    let earth = factories.earthFactory(6300000);
+    app.scene.add(earth);
+
+    let sun = new THREE.DirectionalLight(0xffeeee, 0.8);
+    sun.position.set(0, 0, 1);
+    app.scene.add(sun);
 })();
 
 (document.onresize = () => {
@@ -39,8 +40,16 @@ app.scene.add(app.camera);
 function render() {
     window.requestAnimationFrame(render);
 
+    let skybox = app.background.skybox;
+    skybox.position.set(0, 0, 0);
+    app.camera.localToWorld(skybox.position);
+
     app.controls.update(1 / 60);
     update(app.scene, 1 / 60);
+
+    app.renderer.clear(true, true, true);
+    app.renderer.autoClearColor = false;
+    app.renderer.render(app.background, app.camera);
     app.renderer.render(app.scene, app.camera);
 }
 render();
