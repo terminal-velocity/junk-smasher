fc835dac0c0118b7819a16626d8614febb2f57b3
Matt Randell <ranmat11@gmail.com>
2015-07-28 20:49:22 +0100

    Working movement tracking

--- Client/Script/socket.js
+++ Client/Script/socket.js
@@ -3,62 +3,111 @@
 let $ = require("jquery");
 let io = require("socket.io-client");
 let app = require("./vismod");
+let factories = require("./factories");
 
 $("#loginregisterbutton").html("Sign In");
+
 let socket = io();
- $("#loginregisterbutton").click(function(){
-     socket.emit("register", {username: $("#username").val()});
- });
- $("#username").keyup(function(){
-   socket.emit("checkusername", $("#username").val());
- });
- socket.on("usernamecheckresponse", function(availability){
-   if(availability !== "available"){
-     $("#loginregisterbutton").html("Sign In");
-     $("#login #password").attr("placeholder", "Password");
-   }
-   else {
-     $("#loginregisterbutton").html("Go");
-     $("#login #password").attr("placeholder", "Password (optional)");
-   }
- });
- socket.on("loggedin", function(usernamefromserver){
-   app.username = usernamefromserver;
-   $("#login").fadeOut(function(){
-     $("#teamselection").fadeIn();
-   });
- });
- $("#teamname").keyup(function(){
-   socket.emit("checkteamname", $("#teamname").val());
- });
- socket.on("teamcheckresponse", function(state){
-   if(state === "new"){
-     $("#teamjoin").html("Create Team");
-     $("#teamjoin").fadeIn();
-     $("#teamfull").fadeOut();
-   }
-   else if(state === "exists"){
-     $("#teamjoin").html("Join Team");
-     $("#teamjoin").fadeIn();
-     $("#teamfull").fadeOut();
-   }
-   else if(state === "full"){
-     $("#teamjoin").fadeOut(function(){
-       $("#teamfull").fadeIn();
-     });
-   }
- });
- $("#teamjoin").click(function(){
-   socket.emit("jointeam", $("#teamname").val());
- });
- socket.on("joinedteam", function(){
-   $("#teamselection").fadeOut(function(){
-     $("#teammembers").fadeIn();
-   });
- });
- socket.on("newmember", function(newusername){
-   $("#teammembers>ul").html($("#teammembers>ul").html() + "<li>" + newusername + "</li>");
- });
- socket.on("readytoselect", function(){
-   $("#teammembers #teamready").fadeIn();
- });
+module.exports = socket;
+
+////////////////////////////
+//// jquery  ///////////////
+////////////////////////////
+
+$("#loginregisterbutton").click(function(){
+    socket.emit("register", {
+        username: $("#username").val()
+    });
+});
+
+$("#username").keyup(function(){
+    socket.emit("checkusername", $("#username").val());
+});
+
+$("#teamname").keyup(function(){
+    socket.emit("checkteamname", $("#teamname").val());
+});
+
+$("#teamjoin").click(function(){
+    socket.emit("jointeam", $("#teamname").val());
+});
+
+/////////////////////////////////
+//////// socket.io //////////////
+/////////////////////////////////
+
+socket.on("usernamecheckresponse", function(availability){
+    if(availability !== "available"){
+        $("#loginregisterbutton").html("Sign In");
+        $("#login #password").attr("placeholder", "Password");
+    }
+    else {
+        $("#loginregisterbutton").html("Go");
+        $("#login #password").attr("placeholder", "Password (optional)");
+    }
+});
+
+socket.on("loggedin", function(usernamefromserver) {
+    app.username = usernamefromserver;
+    $("#login").fadeOut(function(){
+        $("#teamselection").fadeIn();
+    });
+});
+
+socket.on("teamcheckresponse", function(state){
+    if(state === "new"){
+        $("#teamjoin").html("Create Team");
+        $("#teamjoin").fadeIn();
+        $("#teamfull").fadeOut();
+    }
+    else if(state === "exists"){
+        $("#teamjoin").html("Join Team");
+        $("#teamjoin").fadeIn();
+        $("#teamfull").fadeOut();
+    }
+    else if(state === "full"){
+        $("#teamjoin").fadeOut(function(){
+            $("#teamfull").fadeIn();
+        });
+    }
+});
+
+socket.on("joinedteam", function(){
+    $("#teamselection").fadeOut(function(){
+        $("#teammembers").fadeIn();
+    });
+
+    window.setInterval(() => {
+        socket.emit("position-update-user", {
+            position: [
+                app.camera.spherical.lat,
+                app.camera.spherical.lon,
+                app.camera.spherical.elev
+            ]
+        });
+    }, 100);
+});
+
+socket.on("newmember", function(newusername){
+    $("#teammembers>ul").html($("#teammembers>ul").html() + "<li>" + newusername + "</li>");
+    if (newusername === app.username) { return; }
+
+    console.log("Received new user data for " + newusername);
+    app.scene.add(factories.playerFactory([0, 0, 7000000], newusername, app));
+});
+
+socket.on("position-update-others", function (data) {
+    console.log("position update:");
+    console.log(data);
+
+    let userMesh = app.players[data.user];
+    userMesh.spherical = {
+        lat: data.position[0],
+        lon: data.position[1],
+        elev: data.position[2]
+    };
+});
+
+socket.on("readytoselect", function(){
+    $("#teammembers #teamready").fadeIn();
+});
