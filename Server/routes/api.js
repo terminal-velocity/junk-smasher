83be67d27678b489d6f8200e9994aeb124d5ad7c
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2015-07-29 10:22:20 +0000

    Submit leaderboard scores

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -2,26 +2,44 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/api/test", function(req, res){
-	res.send("Hello, test");
+	res.send("Hello, test");scores
 });
 
-/*
-router.get("/api/new", function(req, res){
-  var saves = req.db.collection('saves');
-  var save = {};
-  if(!req.query.level){
-    save.level = 0;
+
+router.get("/api/user-scores/new", function(req, res){
+  var scores = req.db.collection('user-scores');
+  var score = {};
+  if(!req.query.score){
+    score.score = 0;
   }else{
-    save.level = req.query.level;
+    score.score = parseInt(req.query.score);
   }
-  save.username = req.query.username;
-  saves.insert(save, function(err, doc) {
+  score.name = req.query.name;
+  scores.insert(score, function(err, doc) {
     if(err){
       res.json({error: true, message: "ERROR Error adding data."+err});
     }else{
       res.json({error: false, id: doc._id , message: "SUCCESS "+doc._id});
     }
   });
-});*/
+});
+
+router.get("/api/team-scores/new", function(req, res){
+  var scores = req.db.collection('team-scores');
+  var score = {};
+  if(!req.query.score){
+    score.score = 0;
+  }else{
+    score.score = parseInt(req.query.score);
+  }
+  score.name = req.query.name;
+  scores.insert(score, function(err, doc) {
+    if(err){
+      res.json({error: true, message: "ERROR Error adding data."+err});
+    }else{
+      res.json({error: false, id: doc._id , message: "SUCCESS "+doc._id});
+    }
+  });
+});
 
 module.exports = router;
