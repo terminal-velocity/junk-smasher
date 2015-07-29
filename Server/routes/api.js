60d33fb1d66062187a3c28295c6c28fcec8bc9f9
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2015-07-29 10:46:16 +0100

    Update api.js

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -5,4 +5,22 @@ router.get("/test", function(req, res){
 	res.send("Hello, test");
 });
 
+router.get("/new", function(req, res){
+  var saves = req.db.collection('saves');
+  var save = {};
+  if(!req.query.level){
+    save.level = 0;
+  }else{
+    save.level = req.query.level;
+  }
+  save.username = req.query.username;
+  saves.insert(save, function(err, doc) {
+    if(err){
+      res.json({error: true, message: "ERROR Error adding data."+err});
+    }else{
+      res.json({error: false, id: doc._id , message: "SUCCESS "+doc._id});
+    }
+  });
+});
+
 module.exports = router;
