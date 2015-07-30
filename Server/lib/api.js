e6341df410b0a72b185dc6c3c42c599a857679dd
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2015-07-30 11:40:10 +0100

    Create api.js

---
+++ Server/lib/api.js
@@ -0,0 +1,29 @@
+exports.addUserScore = function(db, name, value){
+  var scores = db.collection('user-scores');
+  var score = {};
+  score.score = parseInt(value);}
+  score.name = req.query.name;
+  scores.insert(score, function(err, doc) {
+    if(err){
+      console.log(JSON.stringify({error: true, message: "ERROR Error adding data."+err}));
+    }else{
+      console.log(JSON.stringify({error: false, id: doc._id , message: "SUCCESS "+doc._id}));
+    }
+  });
+  return;
+}
+
+exports.addTeamScore = function(db, name, value){
+  var scores = db.collection('team-scores');
+  var score = {};
+  score.score = parseInt(value);
+  score.name = req.query.name;
+  scores.insert(score, function(err, doc) {
+    if(err){
+      console.log(JSON.stringify({error: true, message: "ERROR Error adding data."+err}));
+    }else{
+      console.log(JSON.stringify({error: false, id: doc._id , message: "SUCCESS "+doc._id}));
+    }
+  });
+  return;
+}
