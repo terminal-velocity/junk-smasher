56d3d01e1e5693a4adebead8a8f0bc9a5d60aaf9
Matt Randell <versioncontrol@hexaglow.com>
2015-08-01 12:43:40 +0100

    comment main.js

--- Client/Script/main.js
+++ Client/Script/main.js
@@ -1,18 +1,24 @@
 "use strict";
 //const angular = require("angular");
 
+// Visualisation module, used for the THREE stuff.
 let app = require("./vismod");
 
+// Dom manipulation and factory functions respectively.
 const $ = require("jquery"),
     factories = require("./factories");
 
+// Communication with the server
 require("./socket");
 
+// Fetch the dataset.
 $.get("/data/doc-min-1.geojson", (data) => {
+    // If string, parse into object
     if (typeof data === typeof "") {
         data = JSON.parse(data);
     }
 
+    // points - global for debugging purposes.
     let points = window.points = [];
     console.log("There are " + data.features.length);
     for (let i = 0; i < data.features.length; i++) {
@@ -21,6 +27,8 @@ $.get("/data/doc-min-1.geojson", (data) => {
             .toLowerCase().indexOf("active") !== -1;
         point.jid = data.features[i].id;
 
+        // Filter points based on approximate proximity to
+        // the player's spawn location.
         if (
             (point[0] >= app.meta.location.lat - 30)
             && (point[0] <= app.meta.location.lat + 30)
@@ -37,6 +45,7 @@ $.get("/data/doc-min-1.geojson", (data) => {
     for (let i = 0; i < points.length; i++) {
         let point = points[i];
 
+        // construct the cubes used to display the items.
         let mesh = factories.junkFactory(point[0], point[1], point[2],
             point.active);
         mesh.meta = {
